803d742944019d9cb0e7ebbd8c8365dd2048a626
Drewpb333 <34869642+Drewpb333@users.noreply.github.com>
2020-01-25 08:59:33 -0500

    added save_workbook and get_earnings_table functions

--- index.py
+++ index.py
@@ -15,3 +15,13 @@ def create_heading():
     heading = ['Date', 'Symbol', 'Company', 'Call Time', 'EPS Estimate', 'Reported EPS', 'Surpise(%)']
     for i in range(len(heading)):
         sheet1.write(0, i, heading[i])
+
+def save_workbook(file_name):
+    wb.save(file_name)
+
+def get_earnings_table(monday_date, friday_date, earnings_date):
+    url = 'https://finance.yahoo.com/calendar/earnings?from={}&to={}&day={}'.format(monday_date, friday_date, earnings_date)
+    print(url)
+    html_get = requests.get(url)
+    soup = BeautifulSoup(html_get.text, "html.parser")
+    return soup.table
