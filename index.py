1858bd5ff49932050d0dacb3a13a60d5dbaed162
Drewpb333 <34869642+Drewpb333@users.noreply.github.com>
2020-01-25 08:55:29 -0500

    added create_workbook, create_worksheet, and create_heading functions

--- index.py
+++ index.py
@@ -4,3 +4,14 @@ import xlwt
 from xlwt import Workbook
 import datetime
 import calendar
+
+def create_workbook():
+    return Workbook()
+
+def create_worksheet():
+    return wb.add_sheet("Sheet1")
+
+def create_heading():
+    heading = ['Date', 'Symbol', 'Company', 'Call Time', 'EPS Estimate', 'Reported EPS', 'Surpise(%)']
+    for i in range(len(heading)):
+        sheet1.write(0, i, heading[i])
